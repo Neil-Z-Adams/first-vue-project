dc96523d164f40f44ea7a304adde09a355494275
Neil Adams <144927792+lovelykiller0430@users.noreply.github.com>
2023-09-26 02:49:30 -0700

    first vue project completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductsView from '../views/ProductsView.vue'
+import PastOrders from '../views/PastOrders.vue'
 
 const routes = [
   {
@@ -13,7 +15,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: {}
+    component: ProductsView
+  },
+  {
+    path: '/past-orders',
+    name: 'Past-orders',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: PastOrders
   }
 ]
 
